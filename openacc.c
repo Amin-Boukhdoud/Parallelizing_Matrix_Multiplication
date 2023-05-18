b7baa53aae9ec4fcd57725b0f521ac07951c5832
Amin-Boukhdoud <126620070+Amin-Boukhdoud@users.noreply.github.com>
2023-05-19 01:49:55 +0300

    Add files via upload

---
+++ openacc.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <openacc.h>
+
+int main() {
+    int N = 1024; // number of rows in M and P
+    int M = 512; // number of columns in M and number of rows in N
+    int K = 2048; // number of columns in N and P
+
+    float** MM = (float**)malloc(N * sizeof(float*));
+    for (int i = 0; i < N; i++) {
+        MM[i] = (float*)malloc(M * sizeof(float));
+        for (int j = 0; j < M; j++) {
+            MM[i][j] = rand() / (float)RAND_MAX;
+        }
+    }
+
+    float** NN = (float**)malloc(M * sizeof(float*));
+    for (int i = 0; i < M; i++) {
+        NN[i] = (float*)malloc(K * sizeof(float));
+        for (int j = 0; j < K; j++) {
+            NN[i][j] = rand() / (float)RAND_MAX;
+        }
+    }
+
+    float** R = (float**)malloc(N * sizeof(float*));
+    for (int i = 0; i < N; i++) {
+        R[i] = (float*)malloc(K * sizeof(float));
+    }
+
+    clock_t start_time = clock();
+    // MM
+    #pragma acc data copyin(MM[0:N][0:M], NN[0:M][0:K]), copyout(R[0:N][0:K])
+    {
+        #pragma acc parallel loop collapse(2)
+        for(int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                R[i][j] = 0;
+                for(int k = 0; k < M; k++) {
+                    R[i][j] += MM[i][k] * NN[k][j];
+                }
+            }
+        }
+    }
+    clock_t end_time = clock();
+
+    // for(int i = 0; i < N; i++) {
+    //     for (int j = 0; j < K; j++) {
+    //         printf("%f ", R[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    double duration = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("Execution time: %lf seconds.\n", duration);
+
+    return 0;
+}
